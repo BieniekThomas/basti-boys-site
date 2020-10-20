f02cdfa9d9e3b9ef319eb68e5e31f709df4c9392
Thomas Bieniek <thomas.bieniek@adesso.de>
2020-10-20 12:57:16 +0200

    init

--- app/pages/home.js
+++ app/pages/home.js
@@ -1,10 +1,90 @@
-import React from 'react';
-import Wrapper from '../util/wrapper/wrapper';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+// import Wrapper from '../util/wrapper/wrapper';
 
 const HomePage = () => {
-	console.log( 'Welcome' );
+	const [hasClicked, setHasClicked] = useState( false );
 
-	return <Wrapper><div>Hey there!</div></Wrapper>;
+	let variantsChildren = {
+		hidden: { opacity: 0, x: -100 },
+		show: { opacity: 1, x: 0 },
+	};
+
+	const fillScreenWithFonts = () => {
+		const content = [];
+		for ( let i = 0; i < 5; i += 1 ) {
+			const show = {
+				opacity: 1,
+				x: 3 * -i,
+			};
+			variantsChildren = {
+				...variantsChildren,
+				show: {
+					...show,
+				},
+			};
+			content.push(
+				<motion.h1
+					initial="hidden"
+					animate="visible"
+					key={i}
+					variants={variantsChildren}
+				>
+					KLIPPING
+				</motion.h1>,
+			);
+		}
+		return content;
+	};
+
+	const content = fillScreenWithFonts();
+
+	const stagger = {
+		hidden: { opacity: 0 },
+		show: {
+			opacity: 1,
+			scale: hasClicked ? 500 : 1,
+			transition: {
+				delayChildren: 0.8,
+				staggerDirection: 1,
+			},
+		},
+	};
+
+	const onClickHandler = () => {
+		console.log( 'clicked' );
+		setHasClicked( true );
+	};
+
+	return (
+		<div style={{
+			display: 'flex',
+			height: '100vh',
+			alignItems: 'center',
+			justifyContent: 'center',
+			minHeight: '100vh',
+			maxHeight: '100vh',
+		}}
+		>
+			<motion.div
+				initial="hidden"
+				animate="show"
+				variants={stagger}
+				href="/about"
+				onTap={() => onClickHandler()}
+				whileHover={{
+					scale: 1.2,
+					transition: { duration: 1 },
+				}}
+				drag
+				dragConstraints={{
+					left: -50, right: 50, top: 50, bottom: 50,
+				}}
+			>
+				{content.map( item => item )}
+			</motion.div>
+		</div>
+	);
 };
 
 export default HomePage;
